c1c2bd530fdbf5d2f9f7b422a3ba609f5f73c419
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-04 23:29:31 +0200

    Cv component added

---
+++ src/components/cv/Cv/Cv.js
@@ -0,0 +1,18 @@
+import Background from "../Background/Background";
+import LeftPanel from "../LeftPanel.js";
+import RightPanel from "../RightPanel.js";
+import "./Cv.css";
+
+const Cv = () => {
+
+    return(
+        <section>
+            <Background />
+            <LeftPanel />
+            <RightPanel />
+        </section>
+    )
+
+};
+
+export default Cv
