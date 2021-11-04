b44458bb12b55c9586cd3afa34fe3318f352c663
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-04 22:37:34 +0200

    Circles added

---
+++ src/components/cv/Background/Circles/Circles.js
@@ -0,0 +1,43 @@
+import Circle from '../../Circle/Circle';
+import './Circles.css';
+
+const Circles = () => {
+    return(
+        <div>
+            <Circle fill="black" />
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="white"/>
+            <Circle fill="black"/>
+            <Circle fill="white"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="red"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="red"/>
+            <Circle fill="red"/>
+            <Circle fill="red"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+            <Circle fill="black"/>
+        </div>
+    )
+}
+
+export default Circles
