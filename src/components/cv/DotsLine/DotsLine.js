8dd371a137ba508e06190b5fa79e4de8bfb18e86
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-05 23:17:06 +0200

    DotsLine added

---
+++ src/components/cv/DotsLine/DotsLine.js
@@ -0,0 +1,16 @@
+import './DotsLine.css';
+
+const DotsLine = () => {
+    return(
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+
+export default DotsLine
