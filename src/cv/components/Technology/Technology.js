f9f68c211470c7b49849db161bde0ce6e2446023
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-06 23:47:12 +0200

    Technology added

---
+++ src/cv/components/Technology/Technology.js
@@ -0,0 +1,11 @@
+import './Technology.css';
+
+const Technology = ({ icon }) => {
+    return(
+        <div className='technology'>
+            {icon}
+        </div>
+    )
+}
+
+export default Technology
