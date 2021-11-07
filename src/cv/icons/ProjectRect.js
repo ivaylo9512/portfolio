f9d501d965c01141e8d5915ecfa79c314053dbc2
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-07 23:52:26 +0200

    ProjectRect + TechnologiesSvg added

---
+++ src/cv/icons/ProjectRect.js
@@ -0,0 +1,7 @@
+const ProjectRect = () => {
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
+        <rect width="100%" height="100%" rx="10" ry="10"/>
+    </svg>
+}
+
+export default ProjectRect
