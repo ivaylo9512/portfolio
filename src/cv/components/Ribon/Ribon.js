3622f1f95ed908fad84338bbb14f6ac819ce44bd
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-07 22:45:03 +0200

    Ribon styling + name/children added

--- src/cv/components/Ribon/Ribon.js
+++ src/cv/components/Ribon/Ribon.js
@@ -1,10 +1,15 @@
+import Rect from 'cv/components/Rect';
 import './Ribon.css';
 
-const Ribon = ({ name, color, children }) => {
+const Ribon = ({ name, children }) => {
     return(
-        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 564 95">
-            <rect width="100%" height="100%" rx="10" ry="10"></rect>
-        </svg>
+        <div className="ribon">
+            <div className="content">
+                {name}
+                {children}
+            </div>
+            <Rect />
+        </div>
     )
 }
 
