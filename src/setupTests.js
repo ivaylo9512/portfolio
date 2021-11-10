5407b7a2c7f52f651e6922af10b6f801d4038da3
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-10 22:59:41 +0200

    setupTest added

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,15 @@
-import '@testing-library/jest-dom';
+import Enzyme, { ShallowWrapper, ReactWrapper } from "enzyme";
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+});
+
+function findByTestid(id) {
+  return this.find(`[data-testid='${id}']`)
+}
+
+ShallowWrapper.prototype.findByTestid = findByTestid;
+ReactWrapper.prototype.findByTestid = findByTestid;
+
+jest.setTimeout(20000);
