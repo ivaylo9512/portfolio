33c145c7565e90dcd374d79f687a311062601660
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-10 23:55:43 +0200

    Project + List snapshot tests added

---
+++ src/cv/components/List/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import List from "../List";
+import { shallow } from 'enzyme';
+
+describe('List snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <List />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
---
+++ src/cv/components/Project/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import Project from "../Project";
+import { shallow } from 'enzyme';
+
+describe('Project snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <Project />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
