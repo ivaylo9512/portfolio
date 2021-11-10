b797b772afe81e4ab7f5eda3f380b617fde66646
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-10 23:10:56 +0200

    Technology snapshot tests added

---
+++ src/cv/components/Technology/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import Technology from "../Technology";
+import { shallow } from 'enzyme';
+
+describe('Technology snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <Technology />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
