94b58dbce39f567d3d6d95efa81f0a8d28db40e2
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-10 23:56:05 +0200

    Languages + LeftPanel snapshot tests added

---
+++ src/cv/components/Languages/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import Languages from "../Languages";
+import { shallow } from 'enzyme';
+
+describe('Languages snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <Languages />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
---
+++ src/cv/components/LeftPanel/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import LeftPanel from "../LeftPanel";
+import { shallow } from 'enzyme';
+
+describe('LeftPanel snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <LeftPanel />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
