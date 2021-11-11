504e742b39c80e7ddea0739f8777bc51b014aab6
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-11 23:25:14 +0200

    Circles snapshot tests added

---
+++ src/cv/components/Circles/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import Circles from "../Circles";
+import { shallow } from 'enzyme';
+
+describe('Circles snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <Circles />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+     
+        expect(wrapper).toMatchSnapshot();
+    })
+});
