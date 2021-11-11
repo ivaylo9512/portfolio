d35243360a90a9ffe895cfe789b2566b77bbaa89
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-11 23:44:57 +0200

    Project content added + array of projects

--- src/cv/components/Project/Project.js
+++ src/cv/components/Project/Project.js
@@ -1,14 +1,26 @@
-import ProjectRect from "cv/icons/ProjectRect";
 import Circle from "cv/icons/Circle";
 import "./Project.scss";
+import Github from "svgIcons/Github";
+import Rect from "cv/icons/Rect";
 
-const Project = ({ ratio }) => {
+const Project = ({ height, width, name, repo, info }) => {
     return(
         <div className="project">
+            <div className="content">
+                <p className="name">{name}</p>
+                <div className="github-container">
+                    <Github />
+                    <a href={`https://github.com/ivaylo9512/${repo}`}>Github</a>
+                </div>
+                <div className="info">
+                    <Circle />
+                    <p>{info}</p>
+                </div>
+            </div>
             <div className="clip">
                 <Circle />
             </div>
-            <ProjectRect ratio={ratio} />
+            <Rect height={height} width={width} />
         </div>
     )
 }
--- src/cv/components/Projects/Projects.js
+++ src/cv/components/Projects/Projects.js
@@ -4,13 +4,44 @@ import "./Projects.scss";
 const Projects = () => {
     return(
         <div className="projects">
-            <Project ratio={20 / 11}/>
-            <Project ratio={25.6 / 11}/>
-            <Project ratio={20 / 11}/>
-            <Project ratio={20 / 11}/>
-            <Project ratio={20 / 11}/>
+            {projectsInfo.map(p => 
+                <Project height={310} width={310 * 1.6 * p.ratio} name={p.name} repo={p.repo} info={p.info} />)
+            }
         </div>
     )
 }
 
 export default Projects
+
+const projectsInfo = [
+    { 
+        repo: 'restaurant-app-with-chat-long-polling-server', 
+        info: 'Long polling restaurant app with integrated chat',
+        name: 'Restaurant App',
+        ratio: 1
+    },
+    { 
+        repo: 'restaurant-app-with-chat-long-polling-server', 
+        info: 'Desktop client for restaurant app',
+        name: 'Restaurant Client Desktop',
+        ratio: 59.5 / 46
+    },
+    { 
+        repo: 'restaurant-app-with-chat-long-polling-server', 
+        info: 'Chat app with websockets and spring',
+        name: 'Chat App',
+        ratio: 1
+    },
+    { 
+        repo: 'restaurant-app-with-chat-long-polling-server', 
+        info: 'Chat app web application with React',
+        name: 'Chat App Web',
+        ratio: 1
+    },
+    { 
+        repo: 'restaurant-app-with-chat-long-polling-server', 
+        info: 'Task app with graphql and react',
+        name: 'Task App',
+        ratio: 1
+    }
+]
