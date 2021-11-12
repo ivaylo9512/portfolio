80f01d9bdf2a4fc45e5ee7efc9633c0bc62c6789
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-12 23:52:23 +0200

    RedBlob added + BlackBlob to BrownBlob + Extensions removed

---
+++ src/sections/projects/icons/RedBlob.js
@@ -0,0 +1,9 @@
+const BlackBlob = () => {
+    return(
+        <svg className="red-blob" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 593.77 291.72">
+            <path d="M.55,187s38-190,225-187,389,123,366.5,226S-20.45,307,.55,187Z" />
+        </svg>
+    )
+}
+
+export default BlackBlob
