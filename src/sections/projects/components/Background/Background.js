dae681d9398cf9a118eea7981f611105d26a0e31
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-12 23:46:35 +0200

    Background added

---
+++ src/sections/projects/components/Background/Background.js
@@ -0,0 +1,16 @@
+import BrownBlob from "@projects/icons/BrownBlob"
+import RedBlob from "@projects/icons/RedBlob"
+import GreenBlob from "@projects/icons/GreenBlob"
+import "./Background.scss"
+
+const Background = () => {
+    return(
+        <div className="background">
+            <RedBlob />
+            <BrownBlob />
+            <GreenBlob />
+        </div>
+    )
+}
+
+export default Background
