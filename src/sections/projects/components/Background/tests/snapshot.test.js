9173a44da8153e8a7afa3844e7d22a3d891f6b89
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-13 23:53:53 +0200

    Background snapshot test added

---
+++ src/sections/projects/components/Background/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import { shallow } from "enzyme";
+import Background from "../Background"
+
+describe('Background snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <Background />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
