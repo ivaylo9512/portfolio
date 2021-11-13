e1771fdb85c7c1cba1c8ac1bcaebae37c2e9d584
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-13 22:03:22 +0200

    v + animations moved

---
+++ src/sections/projects/svgFilters/ProjectsShadow.js
@@ -0,0 +1,13 @@
+const ProjectsShadow = ({ id }) => {
+    return(
+        <filter id={id} x="-20%" y="-20%" width="160%" height="160%" filterUnits="userSpaceOnUse">
+            <feOffset result="offset" in="SourceAlpha"/>
+            <feGaussianBlur result="blur" stdDeviation="1.236"/>
+            <feFlood result="flood" floodOpacity="0.97"/>
+            <feComposite result="composite" operator="in" in2="blur"/>
+            <feBlend result="blend" in="SourceGraphic"/>
+        </filter>
+    )
+}
+
+export default ProjectsShadow
