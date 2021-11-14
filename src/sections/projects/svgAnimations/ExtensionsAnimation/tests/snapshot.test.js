9ec1361049bd16d0226240274b0be04ba21fd29b
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-14 22:51:53 +0200

    ExtensionsAnimation snapshot tests added

---
+++ src/sections/projects/svgAnimations/ExtensionsAnimation/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import { shallow } from "enzyme";
+import ExtensionsAnimation from "../ExtensionsAnimation"
+
+describe('ExtensionsAnimation snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <ExtensionsAnimation />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
