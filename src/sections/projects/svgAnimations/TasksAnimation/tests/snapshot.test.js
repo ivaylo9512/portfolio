504aca4bb04fed28b9dae7f4d971641d8670ddea
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-14 20:58:17 +0200

    TasksAnimation snapshot tests added

---
+++ src/sections/projects/svgAnimations/TasksAnimation/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import { shallow } from "enzyme";
+import TasksAnimation from "../TasksAnimation"
+
+describe('TasksAnimation snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <TasksAnimation />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
