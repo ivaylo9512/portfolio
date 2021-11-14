4c064602f3b00c51e6dc3272d30c4cbc2a9e7076
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-14 21:21:06 +0200

    ProfileAnimation snapshot tests added

---
+++ src/sections/projects/svgAnimations/ProfileAnimation/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import { shallow } from "enzyme";
+import ProfileAnimation from "../ProfileAnimation"
+
+describe('ProfileAnimation snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <ProfileAnimation />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
