585fee3d329677f9758e4ca58ab6c7f21dcadbad
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-14 23:12:41 +0200

    RestaurantAnimation snapshot tests

---
+++ src/sections/projects/svgAnimations/RestaurantAnimation/tests/snapshot.test.js
@@ -0,0 +1,14 @@
+import { shallow } from "enzyme";
+import RestaurantAnimation from "../RestaurantAnimation"
+
+describe('RestaurantAnimation snapshot tests', () => {
+    const createWrapper = () => shallow(
+        <RestaurantAnimation />
+    )
+
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
+
+        expect(wrapper).toMatchSnapshot();
+    })
+})
