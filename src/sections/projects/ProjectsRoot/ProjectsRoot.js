afbf83bffbc3b85ce3a84932e41e377cf1d27419
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-17 09:37:13 +0200

    Arrow added + projects hide scroll removed

--- src/sections/projects/ProjectsRoot/ProjectsRoot.js
+++ src/sections/projects/ProjectsRoot/ProjectsRoot.js
@@ -1,29 +1,12 @@
 import Background from '@projects/components/Background/Background'
 import AnimationsRoot from '@projects/components/AnimationsRoot/AnimationsRoot'
 import './ProjectsRoot.scss'
-import { useEffect, useRef } from 'react'
 
 const ProjectsRoot = () => {
-    const scrollContainer = useRef();
-    const scroll = useRef();
 
-    useEffect(() => {
-        hideScrollBar();
-        window.addEventListener('resize', hideScrollBar)
-
-        return () => window.removeEventListener('resize', hideScrollBar)
-    }, []) 
-
-
-    const hideScrollBar = (e) => {
-        const scrollHeight = parseFloat(window.getComputedStyle(scroll.current).height)
-        const containerHeight = parseFloat(window.getComputedStyle(scrollContainer.current).height)
-
-        scrollContainer.current.style.height = containerHeight * 2 - scrollHeight + 'px';
-    }
     return(
-        <section className='projects-root' ref={scrollContainer}>
-            <div className='scroll' ref={scroll}>
+        <section className='projects-root'>
+            <div className='scroll'>
                 <Background />
                 <AnimationsRoot />
             </div>
