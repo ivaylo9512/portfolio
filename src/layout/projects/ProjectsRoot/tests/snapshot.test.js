32cc26af0439bc276abf8cce68bc6aac39a961bc
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-18 21:49:50 +0200

    AnimationsRoot/ProjectsRoot snapshot tests updated with useSelector + cases added

--- src/layout/projects/ProjectsRoot/tests/snapshot.test.js
+++ src/layout/projects/ProjectsRoot/tests/snapshot.test.js
@@ -1,13 +1,30 @@
 import { shallow } from 'enzyme';
 import ProjectsRoot from '../ProjectsRoot'
+import * as Redux from 'react-redux';
 
 describe('ProjectsRoot snapshot tests', () => {
-    const createWrapper = () => shallow(
-        <ProjectsRoot />
-    )
+    let selectorSpy;
+
+    beforeEach(() => {
+        selectorSpy = jest.spyOn(Redux, 'useSelector');
+    })
+
+    const createWrapper = (curretnSlide) => {
+        selectorSpy.mockReturnValue(curretnSlide); 
+     
+        return shallow(
+            <ProjectsRoot />
+        )
+    }
 
     it('should match snapshot', () => {
-        const wrapper = createWrapper();
+        const wrapper = createWrapper('first');
+
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    it('should match snapshot with second slide', () => {
+        const wrapper = createWrapper('second');
 
         expect(wrapper).toMatchSnapshot();
     })
--- src/layout/projects/components/AnimationsRoot/tests/snapshot.test.js
+++ src/layout/projects/components/AnimationsRoot/tests/snapshot.test.js
@@ -1,12 +1,29 @@
 import { shallow } from 'enzyme';
-import AnimationsRoot from '../AnimationsRoot'
+import AnimationsRoot from '../AnimationsRoot';
+import * as Redux from 'react-redux';
 
 describe('AnimationsRoot snapshot tests', () => {
-    const createWrapper = () => shallow(
-        <AnimationsRoot />
-    )
+    let selectorSpy;
 
-    it('should match snapshot', () => {
+    beforeEach(() => {
+        selectorSpy = jest.spyOn(Redux, 'useSelector');
+    })
+
+    const createWrapper = (isVisited) => {
+        selectorSpy.mockReturnValue(isVisited);
+
+        return shallow(
+            <AnimationsRoot />
+        )
+    }
+
+    it('should match snapshot with visited', () => {
+        const wrapper = createWrapper('tasks');
+
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    it('should match snapshot without visited', () => {
         const wrapper = createWrapper();
 
         expect(wrapper).toMatchSnapshot();
