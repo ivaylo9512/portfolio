49fb82c0a7fc351e0be23d6a2d7118ea8b2ed9e8
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-19 23:21:30 +0200

    Project link replaced with TabLink + tests that are now tested in the new TabLink component removed from Project

--- src/layout/cv/components/Project/Project.js
+++ src/layout/cv/components/Project/Project.js
@@ -2,19 +2,18 @@ import Circle from '@svgIcons/Circle';
 import './Project.scss';
 import Github from 'svgIcons/Github';
 import Rect from '@svgIcons/Rect';
-import { useSelector } from 'react-redux';
-import { isProjectsSection } from 'app/slices/animationsSlice';
+import TabLink from '@cv/components/TabLink/TabLink';
 
 const Project = ({ height, width, name, repo, info }) => {
-    const isProjects = useSelector(isProjectsSection);
-
     return(
         <div className='project'>
             <div className='content'>
                 <p className='name'>{name}</p>
                 <div className='github-container'>
                     <Github />
-                    <a data-testid='github-link' tabIndex={isProjects ? '-1' : undefined} href={`https://github.com/ivaylo9512/${repo}`}>Github</a>
+                    <TabLink shouldMatch={true} href={`https://github.com/ivaylo9512/${repo}`}>
+                        Github
+                    </TabLink>
                 </div>
                 <div className='info'>
                     <Circle />
--- src/layout/cv/components/Project/tests/snapshot.test.js
+++ src/layout/cv/components/Project/tests/snapshot.test.js
@@ -1,30 +1,13 @@
 import Project from '../Project';
 import { shallow } from 'enzyme';
-import * as Redux from 'react-redux';
 
 describe('Project snapshot tests', () => {
-    let selectorSpy = jest.fn();
+    const createWrapper = () => shallow(
+        <Project />
+    )
 
-    beforeEach(() => {
-        selectorSpy = jest.spyOn(Redux, 'useSelector');
-    })
-
-    const createWrapper = (state) => {
-        selectorSpy.mockReturnValue(state);
-        
-        return shallow(
-            <Project />
-        )
-    }
-
-    it('should match snapshot with projects', () => {
-        const wrapper = createWrapper(true);
-     
-        expect(wrapper).toMatchSnapshot();
-    })
-
-    it('should match snapshot with projects to false', () => {
-        const wrapper = createWrapper(false);
+    it('should match snapshot', () => {
+        const wrapper = createWrapper();
      
         expect(wrapper).toMatchSnapshot();
     })
