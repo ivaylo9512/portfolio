4c0641656c50a71128ad1a88e0716a6f67d47942
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-20 12:16:19 +0200

    more Header/cv styling

--- src/svgIcons/Circle.js
+++ src/svgIcons/Circle.js
@@ -1,6 +1,6 @@
-const Circle = ({ viewBox = '47 47'}) => {
+const Circle = ({ viewBoxUnit = '47'}) => {
     return(
-        <svg className='circle' viewBox={`0 0 ${viewBox}`} xmlns='http://www.w3.org/2000/svg'>
+        <svg className='circle' viewBox={`0 0 ${viewBoxUnit} ${viewBoxUnit}`} xmlns='http://www.w3.org/2000/svg'>
             <circle r='50%' cx='50%' cy='50%'></circle>
         </svg>
     )
