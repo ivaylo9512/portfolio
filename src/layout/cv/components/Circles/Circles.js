314e1d4a912ec2b9060d466deb2551e16c62f48b
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-11-20 07:24:29 +0200

    new way of defining circles + circles styling

--- src/layout/cv/components/Circles/Circles.js
+++ src/layout/cv/components/Circles/Circles.js
@@ -2,40 +2,23 @@ import Circle from '@svgIcons/Circle';
 import './Circles.scss';
 
 const Circles = () => {
+    const c = ({ u = 129, l= 1 }) => Array.from({ length: l}, () => <Circle viewBoxUnit={u}/>)
+
     return(
         <div className='circles'>
-            <Circle fill='black' />
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='white'/>
-            <Circle fill='black'/>
-            <Circle fill='white'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='red'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='red'/>
-            <Circle fill='red'/>
-            <Circle fill='red'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
-            <Circle fill='black'/>
+            <div className='top-project-circles'>
+                {c({ l: 2})}
+            </div>
+            <div className='top-left-circles'>
+                {c({ l: 2})}
+                {c({ l: 1, u: 98})}
+            </div>
+            <div className='top-right-circles'>
+                {c({ l: 3})}
+            </div>
+            <div className='top-spiral'>
+                {c({ l: 6})}
+            </div>
         </div>
     )
 }
