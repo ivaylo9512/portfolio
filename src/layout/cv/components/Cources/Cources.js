5bd22c9dad0900ab8c39e6ed0aa6cd11cb3909b9
ivaylo9512 <petergeorgiev72@gmail.com>
2024-12-18 23:20:56 +0200

    courses updated with new ribon props

--- src/layout/cv/components/Cources/Cources.js
+++ src/layout/cv/components/Cources/Cources.js
@@ -1,34 +1,32 @@
 import List from '@cv/components/List/List'
-import CourcesSvg from '@svgIcons/CourcesSvg'
-import Self from '@svgIcons/Self';
-import Telerik from '@svgIcons/Telerik';
 import Ribon from '@cv/components/Ribon/Ribon';
 import './Cources.scss';
-import Year2016 from '@svgIcons/Year2016';
-import Year2018 from '@svgIcons/Year2018';
 import Rect from '@svgIcons/Rect';
 import Angular from '@svgIcons/Angular';
 import Spring from '@svgIcons/Spring';
-import Year2021 from '@svgIcons/Year2021';
 import ReactSvg from '@svgIcons/ReactSvg';
 
 const Cources = () => {
     return(
-        <List className='cources' name={<CourcesSvg />}>
-            <Ribon name={<Self />}>
-                <Year2016 />
+        <List className='cources' ribon={<span className='work'>Work</span>}>
+            <Ribon name={"Freelance"}>
+                <span>2016</span>
                 <Rect width={30} height={14} radius={3}/>
-                <Year2018 />
+                <span>2018</span>
                 <Angular />
             </Ribon>
-            <Ribon name={<Telerik />}>
-                <Year2018 />
+            <Ribon name={"Telerik"}>
+                <span>2018</span>
                 <Spring />
             </Ribon>
-            <Ribon name={<Self />}>
-                <Year2018 />
+            <Ribon name={"Projects"}>
+                <span>2018</span>
                 <Rect width={30} height={14} radius={3}/>
-                <Year2021 />
+                <span>2022</span>
+                <ReactSvg />
+            </Ribon>
+            <Ribon name={"Hypoport"}>
+                <span>2023</span>
                 <ReactSvg />
             </Ribon>
         </List>
