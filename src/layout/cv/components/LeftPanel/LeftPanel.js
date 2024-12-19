b0414f8ca942a05ad961296b561f04f4e2962320
ivaylo9512 <petergeorgiev72@gmail.com>
2024-12-19 22:50:42 +0200

    left panel height detection added

--- src/layout/cv/components/LeftPanel/LeftPanel.js
+++ src/layout/cv/components/LeftPanel/LeftPanel.js
@@ -1,10 +1,28 @@
 import Projects from '@cv/components/Projects/Projects';
 import MoreGithub from '@cv/components/MoreGithub/MoreGithub';
 import './LeftPanel.scss';
+import { useEffect, useRef } from 'react';
+import useResize from 'AppRoot/hooks/useResize';
 
 const LeftPanel = () => {
+    const projects = useRef();
+    const {dimensions} = useResize();
+
+    useEffect(() => {
+        projects.current.style.height = "unset";
+
+        setTimeout(() => {
+            projects.current.style.height = projects.current.clientHeight + "px";
+            
+        }, 1);
+    }, [dimensions])
+
+    useEffect(() => {
+        projects.current.style.height = projects.current.clientHeight + "px";
+    }, [])
+
     return(
-        <section className='left-panel'>
+        <section className='left-panel' ref={projects}>
             <Projects />
             <MoreGithub shouldEqual={false} />
         </section>
