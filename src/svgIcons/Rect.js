e2284b580a8732cc09cb940bdd8935f34b4eed96
ivaylo9512 <aleksandrovivailo99@gmail.com>
2025-04-23 16:11:39 +0300

    icons change for new layout

--- src/svgIcons/Rect.js
+++ src/svgIcons/Rect.js
@@ -1,8 +1,14 @@
-const Rect = ({ height, width, radius = 10 }) => {
+const Rect = ({ height, width, radius = 10, toggleHeight, isToggle }) => {
     return(
-        <svg className='rect' xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${width} ${height}`} xmlnsXlink='http://www.w3.org/1999/xlink' >
-            <rect width='100%' height='100%' rx={radius} ry={radius}/>
-        </svg>
+        <svg className='rect' xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${width} ${isToggle == undefined ? height : isToggle ? height : toggleHeight}`} xmlnsXlink='http://www.w3.org/1999/xlink' >
+            <rect width='100%' height='100%' rx={radius} ry={radius}> 
+           {isToggle ?
+                <animate attributeName="height" from={isToggle ? height : toggleHeight} to={isToggle ? toggleHeight : height} repeatDur="0.5s" fill="freeze"/>
+                :
+                <animate attributeName="height" from={isToggle ? height : toggleHeight} to={isToggle ? toggleHeight : height} repeatDur="0.5s" fill="freeze"/>
+           }
+            </rect>
+            </svg>
     )
 }
 
