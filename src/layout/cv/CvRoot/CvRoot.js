c832d297892bb3e027d9d58f098ecb647359c391
ivaylo9512 <aleksandrovivailo99@gmail.com>
2025-04-23 16:12:45 +0300

    cvroot changes for new animation

--- src/layout/cv/CvRoot/CvRoot.js
+++ src/layout/cv/CvRoot/CvRoot.js
@@ -4,11 +4,18 @@ import RightPanel from '@cv/components/RightPanel/RightPanel.js';
 import FiltersRoot from '@cv/svgFilters/FiltersRoot/FiltersRoot';
 import Info from '../components/Info/Info';
 import './CvRoot.scss';
+import { useEffect, useState } from 'react';
 
 const CvRoot = () => {
+    const [animated, setAnimated] = useState("animate");
+
+    useEffect(() => {
+        document.documentElement.style.setProperty("--ratio", window.innerHeight / document.body.scrollHeight)
+        setTimeout(() => setAnimated(() => "animated"), 400);
+    }, [])
     return(
         <section className='cv first-style'>
-            <div className='translate-wrapper'>
+            <div className={`translate-wrapper ${animated}`}>
                 <FiltersRoot />
                 <Info />
                 <Background />
